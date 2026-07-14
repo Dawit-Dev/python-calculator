804cf481ad5eb63aa3df2576a6fcd3dca1f7b5b5
Dawit-Dev <100426510+Dawit-Dev@users.noreply.github.com>
2026-07-14 13:53:14 +0100

    Add basic calculator operations

--- calculator.py
+++ calculator.py
@@ -1,1 +1,15 @@
-print("Python Calculator Project Started 🚀")
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+print(add(5, 3))
+print(subtract(10, 4))
+print(multiply(6, 7))
+print(divide(20, 5))
