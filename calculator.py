0d89e79d42987ca91fd869e6ab378ed5cdb7d100
Dawit-Dev <100426510+Dawit-Dev@users.noreply.github.com>
2026-07-20 16:51:01 +0100

    Add main function structure

--- calculator.py
+++ calculator.py
@@ -1,15 +1,6 @@
 from operations import add, subtract, multiply, divide
-history = []
 
-def get_numbers():
-    try:
-        number1 = float(input("Enter first number: "))
-        number2 = float(input("Enter second number: "))
-        return number1, number2
-    
-    except ValueError:
-        print("Please enter valid numbers")
-        return None
+history = []
 
 def show_menu():
         print("===== Python Calculator =====")
@@ -20,7 +11,15 @@ def show_menu():
         print("5. View History")
         print("6. Clear History")
         print("7. Exit")
-
+def get_numbers():
+    try:
+        number1 = float(input("Enter first number: "))
+        number2 = float(input("Enter second number: "))
+        return number1, number2
+    
+    except ValueError:
+        print("Please enter valid numbers")
+        return None
 def show_history(history):
     print("===== Calculation History =====")
 
@@ -34,54 +33,58 @@ def clear_history(history):
     history.clear()
     print("History cleared.")
 
-while True:
-        show_menu()
+def main():
+    while True:
+            show_menu()
 
 
-        choice = input("Choose an operation (1-7): ")
+            choice = input("Choose an operation (1-7): ")
 
-        if choice not in ["1", "2", "3", "4", "5", "6", "7"]:
-            print("Please choose a number between 1 and 7")
-            continue
+            if choice not in ["1", "2", "3", "4", "5", "6", "7"]:
+                print("Please choose a number between 1 and 7")
+                continue
 
-        elif choice == "5":
-            show_history(history)
-            continue
+            elif choice == "5":
+                show_history(history)
+                continue
 
-        elif choice == "6":
-            clear_history(history)
-            continue
+            elif choice == "6":
+                clear_history(history)
+                continue
 
-        elif choice == "7":
-            print("Thank you for using the calculator!")
-            break
+            elif choice == "7":
+                print("Thank you for using the calculator!")
+                break
 
-        numbers = get_numbers()
+            numbers = get_numbers()
 
-        if numbers is None:
-            continue
+            if numbers is None:
+                continue
 
-        number1, number2 = numbers
+            number1, number2 = numbers
 
-        if choice == "1":
-            symbol = "+"
-            result = add(number1, number2)
+            if choice == "1":
+                symbol = "+"
+                result = add(number1, number2)
 
-        elif choice == "2":
-            symbol = "-"
-            result = subtract(number1, number2)
+            elif choice == "2":
+                symbol = "-"
+                result = subtract(number1, number2)
 
-        elif choice == "3":
-            symbol = "*"
-            result = multiply(number1, number2)
+            elif choice == "3":
+                symbol = "*"
+                result = multiply(number1, number2)
 
-        elif choice == "4":
-            symbol = "/"
-            result = divide(number1, number2)
+            elif choice == "4":
+                symbol = "/"
+                result = divide(number1, number2)
 
-        else:
-            print("Invalid choice")
-            continue
+            else:
+                print("Invalid choice")
+                continue
 
-        print("Result:", result)
-        history.append(f"{number1} {symbol} {number2} = {result}")
+            print("Result:", result)
+            history.append(f"{number1} {symbol} {number2} = {result}")
+
+if __name__ == "__main__":
+    main()
